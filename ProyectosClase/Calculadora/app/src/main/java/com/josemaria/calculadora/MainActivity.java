109fe37f95bdb1598198ff8e06ed53554543acb0
Joseverazz <verdezoto.vera@gmail.com>
2021-11-29 20:01:44 +0100

    Actualizacion

--- ProyectosClase/Calculadora/app/src/main/java/com/josemaria/calculadora/MainActivity.java
+++ ProyectosClase/Calculadora/app/src/main/java/com/josemaria/calculadora/MainActivity.java
@@ -3,12 +3,40 @@ package com.josemaria.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0,
+            btnBorrar, btnMas, btnMenos,btnMultiplicar, btnDividir, btnMasMenos, btnModulo, btnIgual;
+
+    private TextField TextResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instancias();
+    }
+
+    private void instancias() {
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btnBorrar = findViewById(R.id.btnBorrar);
+        btnIgual = findViewById(R.id.btnIgual);
+        btnMas = findViewById(R.id.btnMas);
+        btnMasMenos = findViewById(R.id.btnMasMenos);
+        btnMenos = findViewById(R.id.btnMenos);
+        btnMultiplicar = findViewById(R.id.btnMultiplicacion);
+        btnModulo = findViewById(R.id.btnModulo);
+        btnDividir = findViewById(R.id.btnDividir);
     }
 }
--- ProyectosClase/Elementos/app/src/main/java/com/josemaria/elementos/MainActivity.java
+++ ProyectosClase/Elementos/app/src/main/java/com/josemaria/elementos/MainActivity.java
@@ -5,61 +5,102 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity implements OnCheckedChangeListener {
 
-    private ToggleButton toggle;
-    private TextView textToggle,textCheck;
-    private CheckBox check;
-    private Button btnComprobar;
+        private ToggleButton boton;
+        private TextView textoBtn,textoCheck,textoGrupoRadios;
+        private CheckBox check;
+        private Button BtnComprobar;
+        private RadioGroup grupoRadios;
+        private RadioButton radioSeleccionado;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            instancias();
+            acciones();
+        }
+
+        private void acciones() {
+
+            BtnComprobar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    textoBtn.setText(String.valueOf(boton.isChecked()));
+                    textoCheck.setText(String.valueOf(check.isChecked()));
+                    radioSeleccionado = findViewById(grupoRadios.getCheckedRadioButtonId());//selecciona el radio que esta seleccionado
+                    if(radioSeleccionado!=null)
+                    {
+                        textoGrupoRadios.setText(radioSeleccionado.getText());
+                    }
+
+                }
+            });
+            boton.setOnCheckedChangeListener(this);
+            check.setOnCheckedChangeListener(this);
+            grupoRadios.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    radioSeleccionado = (RadioButton)findViewById(checkedId);
+                    textoGrupoRadios.setText(radioSeleccionado.getText());
+                    /*switch (checkedId)
+                    {
+                        case R.id.radio_uno:
+                            textoGrupoRadios.setText("Opcion 1");
+                            break;
+                        case R.id.radio_dos:
+                            textoGrupoRadios.setText("Opcion 2");
+                            break;
+                        case R.id.radio_tres:
+                            textoGrupoRadios.setText("Opcion 3");
+                            break;
+                    }*/
+                }
+            });
 
 
 
+        }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        instancias();
-        acciones();
-    }
+        private void instancias() {
 
-    private void acciones() {
-        btnComprobar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textToggle.setText(String.valueOf(toggle.isChecked()));
-                textCheck.setText(String.valueOf(check.isChecked()));
+            boton = findViewById(R.id.toggle);
+            textoBtn = findViewById(R.id.textBoton);
+            check = findViewById(R.id.check);
+            BtnComprobar = findViewById(R.id.btnComprobar);
+            textoCheck = findViewById(R.id.textCheck);
+            grupoRadios = findViewById(R.id.grupo_radios);
+            textoGrupoRadios = findViewById(R.id.texto_grupo);
+
+
+        }
+
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            switch (buttonView.getId())
+            {
+                case R.id.check:
+                    if(check.isChecked())
+                        textoCheck.setText("Esta checkeado");
+                    else
+                        textoCheck.setText("NO Esta checkeado");
+                    break;
+                case R.id.toggle:
+                    if(check.isChecked())
+                        textoBtn.setText("Boton encendido");
+                    else
+                        textoBtn.setText("Boton apagado");
+                    break;
             }
-        });
-        toggle.setOnCheckedChangeListener(this);
-        check.setOnCheckedChangeListener(this);
-    }
-
-    private void instancias() {
-
-        toggle = findViewById(R.id.toggle);
-        textToggle = findViewById(R.id.text_toggle);
-        textCheck = findViewById(R.id.text_check);
-        check = findViewById(R.id.check);
-        btnComprobar = findViewById(R.id.btn_comprobar);
-
-    }
-
-    @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        switch (buttonView.getId()){
-            case R.id.check:
-                textCheck.setText(String.valueOf(check.isChecked()));
-                break;
-            case R.id.toggle:
-                textToggle.setText(String.valueOf(toggle.isChecked()));
-                break;
         }
     }
-}
